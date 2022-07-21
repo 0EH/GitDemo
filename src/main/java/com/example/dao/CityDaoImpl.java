3cd82be71c9e500666549de31e6f7229ee8b26ad
0EH <netga101310@gmail.com>
2022-07-21 09:43:50 +0900

    test

--- src/main/java/com/example/dao/CityDaoImpl.java
+++ src/main/java/com/example/dao/CityDaoImpl.java
@@ -10,9 +10,9 @@ import com.example.vo.CityVo;
 
 @Repository("cityDao")
 public class CityDaoImpl implements CityDao {
-	
-	@Autowired
-	private SqlSession sqlSession;
+//	
+//	@Autowired
+//	private SqlSession sqlSession;
 
 	@Override
 	public CityVo read(String name) {
